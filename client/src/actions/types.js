f9962792e507d7e7ec1c4575263b1ef525762364
xi-khan <xihan123@gmail.com>
2021-01-17 12:15:35 +0500

    Dahboard, private routing, landing redirect, profile reducer and action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -10,6 +10,7 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
+// Profile stuff
 export const GET_PROFILE = "GET_PROFILE";
 export const GET_PROFILES = "GET_PROFILES";
 export const GET_REPOS = "GET_REPOS";
