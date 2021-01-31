71eb288f1ce83365d253776e3f642818cf1bcd89
xi-khan <xihan123@gmail.com>
2021-01-31 16:45:46 +0500

    nothing

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -37,6 +37,7 @@ function profileReducer(state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
+        profile: null,
       };
     case CLEAR_PROFILE:
       return {
