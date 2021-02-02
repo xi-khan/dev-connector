83bb6fb09190772be25a805e841a1bdb84865617
xi-khan <xihan123@gmail.com>
2021-02-02 18:01:30 +0500

    All done

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,10 @@
-import React, { Fragment, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getPosts } from '../../actions/post';
-import Spinner from '../layout/Spinner';
-import PostItem from './PostItem';
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getPosts } from "../../actions/post";
+import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
           <i className="fas fa-user"></i> Welcome to the community
         </p>
       </h1>
+      <PostForm />
       <div className="posts">
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
